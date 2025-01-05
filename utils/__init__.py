aaaf14949cd11571d2284bd79b73d1040b929332
Abenezer Eshetie <abenezergoo@gmail.com>
2025-01-05 21:04:04 +0300

    create utility for constructing download url, downloading a file, unziping and loading csv files

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+# __version__ = "1.0.0"
+# __author__ = "Abenezer Eshetie"
+from .data_utils import construct_url, download_file, load_file
---
+++ utils/data_utils.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import gdown
+import zipfile
+
+data_output_path = "../data/"
+
+def construct_url(file_id: str) -> str:
+    return f"https://docs.google.com/uc?export=download&id={file_id}"
+
+def download_file(download_url: str, output_path=f"{data_output_path}rossmann_data.zip"):
+    gdown.download(download_url, output_path, quiet=False)
+    with zipfile.ZipFile(output_path, 'r') as zip_ref:
+        zip_ref.extractall(f"{data_output_path}rossmann_data")
+
+
+def load_file() -> pd.DataFrame:
+    csv_file_path = f"{data_output_path}rossmann_data/"
+    store_data = pd.read_csv(f"{csv_file_path}store.csv")
+    train_data = pd.read_csv(f"{csv_file_path}train.csv")
+    test_data = pd.read_csv(f"{csv_file_path}test.csv")
+    sample_submission = pd.read_csv(f"{csv_file_path}sample_submission.csv")
+    return store_data, train_data, test_data, sample_submission
